22cf3aac62f35c2cf506d2020190bae49f828aa5
Ron Miasnik <ron.miasnik@gmail.com>
2019-05-07 23:41:48 -0400

    Initial Commit

---
+++ Princeton Tour/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Princeton Tour
+//
+//  Created by Ron Miasnik on 5/7/19.
+//  Copyright © 2019 Ron Miasnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Princeton TourTests/Princeton_TourTests.swift
@@ -0,0 +1,34 @@
+//
+//  Princeton_TourTests.swift
+//  Princeton TourTests
+//
+//  Created by Ron Miasnik on 5/7/19.
+//  Copyright © 2019 Ron Miasnik. All rights reserved.
+//
+
+import XCTest
+@testable import Princeton_Tour
+
+class Princeton_TourTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
