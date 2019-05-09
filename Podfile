b6bdf094fd9cb5935d394613327e7e2c5aeca632
Ron Miasnik <ron@miasnik.com>
2019-05-09 13:59:15 -0400

    initial commit

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios
+
+target 'Princeton Tour' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Princeton Tour
+
+pod 'ARCL'
+
+  target 'Princeton TourTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Princeton TourUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- Princeton Tour/ViewController.swift
+++ Princeton Tour/ViewController.swift
@@ -7,12 +7,69 @@
 //
 
 import UIKit
+import ARCL
+import CoreLocation
 
 class ViewController: UIViewController {
+    
+    var sceneLocationView = SceneLocationView()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        sceneLocationView.run()
+        
+        // CHECK TO MAKE SURE I'M IN RIGHT LOCATION
+        var locManager = CLLocationManager()
+        locManager.requestWhenInUseAuthorization()
+
+        var currentLocation: CLLocation!
+
+        if( CLLocationManager.authorizationStatus() == .authorizedWhenInUse ||
+            CLLocationManager.authorizationStatus() ==  .authorizedAlways){
+
+            currentLocation = locManager.location
+
+        }
+
+        print("\(currentLocation.coordinate.longitude)")
+        print("\(currentLocation.coordinate.latitude)")
+        print("\(currentLocation.altitude)")
+        
+        let witherspoonCoordinate = CLLocationCoordinate2D(latitude: 40.347502, longitude: -74.660147)
+        let witherspoonLocation = CLLocation(coordinate: witherspoonCoordinate, altitude: 65)
+        let witherspoonImage = UIImage(named: "witherspoon")!
+        
+        let witherspoonAnnotationNode = LocationAnnotationNode(location: witherspoonLocation, image: witherspoonImage)
+        
+        let holderCoordinate = CLLocationCoordinate2D(latitude: 40.348593, longitude: -74.661605)
+        let holderLocation = CLLocation(coordinate: holderCoordinate, altitude: 65)
+        let holderImage = UIImage(named: "holder")!
+        
+        let holderAnnotationNode = LocationAnnotationNode(location: holderLocation, image: holderImage)
+        
+        
+        let blairCoordinate = CLLocationCoordinate2D(latitude: 40.347545, longitude: -74.660961)
+        let blairLocation = CLLocation(coordinate: blairCoordinate, altitude: 65)
+        let blairImage = UIImage(named: "blair")!
+        
+        let blairAnnotationNode = LocationAnnotationNode(location: blairLocation, image: blairImage)
+        
+        
+        sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: witherspoonAnnotationNode)
+        sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: holderAnnotationNode)
+        sceneLocationView.addLocationNodeWithConfirmedLocation(locationNode: blairAnnotationNode)
+
+
+        view.addSubview(sceneLocationView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        sceneLocationView.frame = view.bounds
     }
 
 
